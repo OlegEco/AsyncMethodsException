272b2f37d0b731748401c20d3243343bdf30b717
Oleg <100606637+OlegEco@users.noreply.github.com>
2024-07-27 13:09:01 +0200

    Add project files.

---
+++ AsyncMethodsExeption/Program.cs
@@ -0,0 +1,22 @@
+﻿
+try
+{
+    string url = "https://jsonplaceholder.typicode.com/invalid-url";
+    string data = await FetchDataAsync(url);
+    Console.WriteLine(data);
+}
+catch (HttpRequestException ex)
+{
+    Console.WriteLine($"Request error: {ex.Message}");
+}
+
+async Task<string> FetchDataAsync(string url)
+{
+    using (HttpClient client = new HttpClient())
+    {
+        HttpResponseMessage response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+        string responseData = await response.Content.ReadAsStringAsync();
+        return responseData;
+    }
+}
